a1584c6241a78f47bcdceb58d39c04239511b554
FelipeWong <s29050898@gmail.com>
2024-04-10 10:58:12 +0800

    Revise the GraphCL model

--- GraphCL.py
+++ GraphCL.py
@@ -1,69 +1,292 @@
+import scipy.sparse as sp
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
-# from core.encoders import *
-import json
-from torch import optim
-
-from cortex_DIM.nn_modules.mi_networks import MIFCNet, MI1x1ConvNet
-from losses import *
+from GraphCL_pacakage import (GCN, AvgReadout, Discriminator, Discriminator2,
+                              process, aug)
+from models.dgi_package import process
+from models.model import *
+from hyperparameters.public_hyper import SPACE_TREE
+import pdb
 
 
-class GlobalDiscriminator(nn.Module):
-    def __init__(self, args, input_dim):
-        super().__init__()
-        
-        self.l0 = nn.Linear(32, 32)
-        self.l1 = nn.Linear(32, 32)
+# The model base is from the repository of Shen-Lab folders "unsupervised_Cora_Citeseer"
+# reference: https://proceedings.neurips.cc/paper/2020/file/3fe230348e9a12c13120749e3f9fa4cd-Paper.pdf
 
-        self.l2 = nn.Linear(512, 1)
-    def forward(self, y, M, data):
+class GraphCL_test(nn.Module):
+    def __init__(self, n_in, n_h, activation):
+        super(GraphCL_test, self).__init__()
+        self.gcn = GCN(n_in, n_h, activation)
+        self.read = AvgReadout()
+        self.sigm = nn.Sigmoid()
+        self.disc = Discriminator(n_h)
+        self.disc2 = Discriminator2(n_h)
 
-        adj = Variable(data['adj'].float(), requires_grad=False).cuda()
-        # h0 = Variable(data['feats'].float()).cuda()
-        batch_num_nodes = data['num_nodes'].int().numpy()
-        M, _ = self.encoder(M, adj, batch_num_nodes)
-        # h = F.relu(self.c0(M))
-        # h = self.c1(h)
-        # h = h.view(y.shape[0], -1)
-        h = torch.cat((y, M), dim=1)
-        h = F.relu(self.l0(h))
-        h = F.relu(self.l1(h))
-        return self.l2(h)
+    def forward(self, seq1, seq2, seq3, seq4, adj, aug_adj1, aug_adj2, sparse, msk, samp_bias1, samp_bias2, aug_type):
 
-class PriorDiscriminator(nn.Module):
-    def __init__(self, input_dim):
-        super().__init__()
-        self.l0 = nn.Linear(input_dim, input_dim)
-        self.l1 = nn.Linear(input_dim, input_dim)
-        self.l2 = nn.Linear(input_dim, 1)
+        h_0 = self.gcn(seq1, adj, sparse)
+        if aug_type == 'edge':
 
-    def forward(self, x):
-        h = F.relu(self.l0(x))
-        h = F.relu(self.l1(h))
-        return torch.sigmoid(self.l2(h))
+            h_1 = self.gcn(seq1, aug_adj1, sparse)
+            h_3 = self.gcn(seq1, aug_adj2, sparse)
 
-class FF(nn.Module):
-    def __init__(self, input_dim):
-        super().__init__()
-        # self.c0 = nn.Conv1d(input_dim, 512, kernel_size=1)
-        # self.c1 = nn.Conv1d(512, 512, kernel_size=1)
-        # self.c2 = nn.Conv1d(512, 1, kernel_size=1)
-        self.block = nn.Sequential(
-            nn.Linear(input_dim, input_dim),
-            nn.ReLU(),
-            nn.Linear(input_dim, input_dim),
-            nn.ReLU(),
-            nn.Linear(input_dim, input_dim),
-            nn.ReLU()
-        )
-        self.linear_shortcut = nn.Linear(input_dim, input_dim)
-        # self.c0 = nn.Conv1d(input_dim, 512, kernel_size=1, stride=1, padding=0)
-        # self.c1 = nn.Conv1d(512, 512, kernel_size=1, stride=1, padding=0)
-        # self.c2 = nn.Conv1d(512, 1, kernel_size=1, stride=1, padding=0)
+        elif aug_type == 'mask':
 
-    def forward(self, x):
-        return self.block(x) + self.linear_shortcut(x)
+            h_1 = self.gcn(seq3, adj, sparse)
+            h_3 = self.gcn(seq4, adj, sparse)
+
+        elif aug_type == 'node' or aug_type == 'subgraph':
+
+            h_1 = self.gcn(seq3, aug_adj1, sparse)
+            h_3 = self.gcn(seq4, aug_adj2, sparse)
+
+        else:
+            assert False
+
+        c_1 = self.read(h_1, msk)
+        c_1 = self.sigm(c_1)
+
+        c_3 = self.read(h_3, msk)
+        c_3 = self.sigm(c_3)
+
+        h_2 = self.gcn(seq2, adj, sparse)
+
+        ret1 = self.disc(c_1, h_0, h_2, samp_bias1, samp_bias2)
+        ret2 = self.disc(c_3, h_0, h_2, samp_bias1, samp_bias2)
+
+        ret = ret1 + ret2
+        return ret
+
+    # Detach the return variables
+    def embed(self, seq, adj, sparse, msk):
+        h_1 = self.gcn(seq, adj, sparse)
+        c = self.read(h_1, msk)
+
+        return h_1.detach(), c.detach()
+
+
+"""
+================================================================
+According to the readme of the DEEP-PolyU/NetBenchmark repository, I need to add 
+some subclasses, like check_train_parameters and train_model, to import the new 
+algorithm easily. Besides, the new algorithm should be able to inherit from the base 
+class from ./models/model.py so that we can do tasks 1, 2 & 3 for benchmarking
+================================================================
+"""
+
+
+class GraphCL(Models):
+    @classmethod
+    def is_preprocessing(cls):
+        return False
+
+    @classmethod
+    def is_deep_model(cls):
+        return True
+
+    @classmethod
+    def is_end2end(cls):
+        return False
+
+    def check_train_parameters(self):
+        space_dtree = {"aug_type": hp.choice("aug_type",
+                                             ["edge", "mask", "node"]),
+                       "drop_percent": hp.choice("drop_percent",
+                                                 [0.0, 0.1, 0.2, 0.3, 0.4]),
+                       "lr": SPACE_TREE["lr"],
+                       "nb_epochs": SPACE_TREE["nb_epochs"]}
+        return space_dtree
+
+    def train_model(self, **kwargs):
+        aug_type = kwargs["aug_type"]
+        drop_percent = int(kwargs["drop_percent"])
+        np.random.seed(42)
+        torch.manual_seed(42)
+        if self.use_gpu:
+            device = self.device
+            torch.cuda.manual_seed(42)
+        else:
+            device = self.device
+            print("--> No GPU")
+
+        # training params
+        # print(int(kwargs["batch_size"]))
+        batch_size = 1
+        nb_epochs = int(kwargs["nb_epochs"])
+        patience = 20
+        lr = kwargs["lr"]
+        l2_coef = 0.0
+        drop_prob = 0.0
+        hid_units = 512
+        sparse = True
+
+        nonlinearity = 'prelu'  # special name to separate parameters
+        adj, features, labels, idx_train, idx_val, idx_test = process.load_citationmat(self.mat_content)
+        features, _ = process.preprocess_features(features)
+
+        nb_nodes = features.shape[0]  # node number
+        ft_size = features.shape[1]  # node features dim
+        nb_classes = labels.shape[1]  # classes = 6
+
+        features = torch.FloatTensor(features[np.newaxis])
+
+        '''
+        ------------------------------------------------------------
+        edge node mask subgraph
+        ------------------------------------------------------------
+        '''
+        print("Begin Aug:[{}]".format(aug_type))
+        if aug_type == 'edge':
+
+            aug_features1 = features
+            aug_features2 = features
+
+            aug_adj1 = aug.aug_random_edge(adj, drop_percent=drop_percent)  # random drop edges
+            aug_adj2 = aug.aug_random_edge(adj, drop_percent=drop_percent)  # random drop edges
+
+        elif aug_type == 'node':
+
+            aug_features1, aug_adj1 = aug.aug_drop_node(features, adj, drop_percent=drop_percent)
+            aug_features2, aug_adj2 = aug.aug_drop_node(features, adj, drop_percent=drop_percent)
+
+        elif aug_type == 'subgraph':
+
+            aug_features1, aug_adj1 = aug.aug_subgraph(features, adj, drop_percent=drop_percent)
+            aug_features2, aug_adj2 = aug.aug_subgraph(features, adj, drop_percent=drop_percent)
+
+        elif aug_type == 'mask':
+
+            aug_features1 = aug.aug_random_mask(features, drop_percent=drop_percent)
+            aug_features2 = aug.aug_random_mask(features, drop_percent=drop_percent)
+
+            aug_adj1 = adj
+            aug_adj2 = adj
+
+        else:
+            assert False
+
+        '''
+        ------------------------------------------------------------
+        '''
+
+        adj = process.normalize_adj(adj + sp.eye(adj.shape[0]))
+        aug_adj1 = process.normalize_adj(aug_adj1 + sp.eye(aug_adj1.shape[0]))
+        aug_adj2 = process.normalize_adj(aug_adj2 + sp.eye(aug_adj2.shape[0]))
+
+        if sparse:
+            sp_adj = process.sparse_mx_to_torch_sparse_tensor(adj)
+            sp_aug_adj1 = process.sparse_mx_to_torch_sparse_tensor(aug_adj1)
+            sp_aug_adj2 = process.sparse_mx_to_torch_sparse_tensor(aug_adj2)
+
+        else:
+            adj = (adj + sp.eye(adj.shape[0])).todense()
+            aug_adj1 = (aug_adj1 + sp.eye(aug_adj1.shape[0])).todense()
+            aug_adj2 = (aug_adj2 + sp.eye(aug_adj2.shape[0])).todense()
+
+        '''
+        ------------------------------------------------------------
+        mask
+        ------------------------------------------------------------
+        '''
+
+        '''
+        ------------------------------------------------------------
+        '''
+        if not sparse:
+            adj = torch.FloatTensor(adj[np.newaxis])
+            aug_adj1 = torch.FloatTensor(aug_adj1[np.newaxis])
+            aug_adj2 = torch.FloatTensor(aug_adj2[np.newaxis])
+
+        labels = torch.FloatTensor(labels[np.newaxis])
+        idx_train = torch.LongTensor(idx_train)
+        idx_val = torch.LongTensor(idx_val)
+        idx_test = torch.LongTensor(idx_test)
+
+        model = GraphCL_test(ft_size, hid_units, nonlinearity)
+        optimiser = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=l2_coef)
+
+        if torch.cuda.is_available():
+            print('Using CUDA')
+            model.cuda(device)
+            features = features.cuda(device)
+            aug_features1 = aug_features1.cuda(device)
+            aug_features2 = aug_features2.cuda(device)
+            if sparse:
+                sp_adj = sp_adj.cuda(device)
+                sp_aug_adj1 = sp_aug_adj1.cuda(device)
+                sp_aug_adj2 = sp_aug_adj2.cuda(device)
+            else:
+                adj = adj.cuda(device)
+                aug_adj1 = aug_adj1.cuda(device)
+                aug_adj2 = aug_adj2.cuda(device)
+
+            labels = labels.cuda(device)
+            idx_train = idx_train.cuda(device)
+            idx_val = idx_val.cuda(device)
+            idx_test = idx_test.cuda(device)
+
+        b_xent = nn.BCEWithLogitsLoss()
+        xent = nn.CrossEntropyLoss()
+        cnt_wait = 0
+        best = 1e9
+        best_t = 0
+        best_model = None
+        start_time = time.time()
+
+        for epoch in range(nb_epochs):
+
+            model.train()
+            optimiser.zero_grad()
+
+            idx = np.random.permutation(nb_nodes)
+            shuf_fts = features[:, idx, :]
+
+            lbl_1 = torch.ones(batch_size, nb_nodes)
+            lbl_2 = torch.zeros(batch_size, nb_nodes)
+            lbl = torch.cat((lbl_1, lbl_2), 1)
+
+            if torch.cuda.is_available():
+                shuf_fts = shuf_fts.cuda(device)
+                lbl = lbl.cuda(device)
+
+            logits = model(features, shuf_fts, aug_features1, aug_features2,
+                           sp_adj if sparse else adj,
+                           sp_aug_adj1 if sparse else aug_adj1,
+                           sp_aug_adj2 if sparse else aug_adj2,
+                           sparse, None, None, None, aug_type=aug_type)
+
+            loss = b_xent(logits, lbl)
+            print('Loss:[{:.4f}]'.format(loss.item()))
+
+            if loss < best:
+                best = loss
+                best_t = epoch
+                cnt_wait = 0
+                # torch.save(model.state_dict(), args.save_name)
+                best_model = model.state_dict()
+            else:
+                cnt_wait += 1
+
+            if cnt_wait == patience:
+                print('Early stopping!')
+                break
+
+            loss.backward()
+            optimiser.step()
+
+        print('Loading {}th epoch'.format(best_t))
+        # model.load_state_dict(torch.load(args.save_name))
+        model.load_state_dict(best_model)
+
+        embeds, _ = model.embed(features, sp_adj if sparse else adj, sparse, None)
+
+        # train_embs = embeds[0, idx_train]
+        # val_embs = embeds[0, idx_val]
+        # test_embs = embeds[0, idx_test]
+
+        node_emb = embeds.data.cpu().numpy()
+        # print('node_shape ', node_emb.shape)
+        node_emb = node_emb.reshape(node_emb.shape[1:])
+        # print('node_shape_new ', node_emb.shape)
+
+        return node_emb
 
