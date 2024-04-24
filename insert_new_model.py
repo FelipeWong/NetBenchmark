c96930df46e0fdcce775eeefd7617635846f238e
FelipeWong <s29050898@gmail.com>
2024-04-24 19:06:44 +0800

    Add command for running new model & add result for task 1

--- insert_new_model.py
+++ insert_new_model.py
@@ -6,9 +6,15 @@ new_models = [GraphCL]
 """
 python netBenchmark.py --method=graphcl --task_method=task1 --tuning_method=random
 
-python netBenchmark.py --task_method=task1 --training_ratio=0.1 --tuning_method=random
+python netBenchmark.py --method=graphcl --task_method=task1 --training_ratio=0.1 --tuning_method=random
 
-python netBenchmark.py --method=graphcl --task_method=task2 --tuning_method=random
+python netBenchmark.py --method=graphcl --task_method=task1 --training_ratio=0.1 --tuning_method=tpe
 
-python netBenchmark.py --method=graphcl --task_method=task3 --tuning_method=random
+python netBenchmark.py --method=graphcl --task_method=task2 --training_ratio=0.1 --tuning_method=random
+
+python netBenchmark.py --method=graphcl --task_method=task2 --training_ratio=0.1 --tuning_method=tpe
+
+python netBenchmark.py --method=graphcl --task_method=task3 --training_ratio=0.1 --tuning_method=random
+
+python netBenchmark.py --method=graphcl --task_method=task3 --training_ratio=0.1 --tuning_method=tpe
 """
